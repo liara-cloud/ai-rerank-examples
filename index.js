cc3a02eeedad768292ee6d38b6f61de8b0d3b74b
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2026-09-20 10:35:54 +0330

    make ready for production

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
-const response = await fetch("<BASE_URL>/rerank", {
-  method: "POST",
-  headers: {
-    Authorization: "Bearer <LIARA_API_KEY>",
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    model: "<RERANK_MODEL_NAME>",
-    query: "And who is God?",
+// npm install dotenv
+require("dotenv").config();
 
-    documents: [
+async function main() {
+  try {
+    const response = await fetch(`${process.env.BASE_URL}/rerank`, {
+      method: "POST",
 
-        "God means love, purity, intimacy, friendship",
-        "God is kind",
-        "God loves us and we should love him too",
-        "AI means Artificial Intelligence",
+      headers: {
+        Authorization: `Bearer ${process.env.LIARA_API_KEY}`,
+        "Content-Type": "application/json",
+      },
 
-    ],
-    top_n: 3,
-  }),
-});
+      body: JSON.stringify({
+        model: process.env.RERANK_MODEL_NAME,
 
-const data = await response.json();
-console.log(data);
+        query: "And who is God?",
+
+        documents: [
+          "God means love, purity, intimacy, friendship",
+          "God is kind",
+          "God loves us and we should love him too",
+          "AI means Artificial Intelligence",
+        ],
+
+        top_n: 3,
+      }),
+    });
+
+    if (!response.ok) {
+      const error = await response.text();
+      throw new Error(`Request failed: ${response.status} - ${error}`);
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Node.JS + rerank models
+
+## Getting Started
+
+```
+git clone https://github.com/liara-cloud/ai-rerank-examples.git
+```
+
+```
+cd ai-rerank-examples
+```
+
+```
+git checkout nodejs
+```
+
+```
+mv .env.example .env # and set ENVs
+```
+
+```
+npm i
+```
+
+```
+node index.js # and done ... 
+```
